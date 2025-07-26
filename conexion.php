fb65349e394d9838ef704f4c6017fed96feeb58d
crepitob <crepitob@gmail.com>
2025-07-25 23:26:33 -0300

    LOGRADO

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "cooperadoraenu");
+$conexion = new mysqli("localhost", "root", "2612sanTI76+", "cooperadoraenu");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
